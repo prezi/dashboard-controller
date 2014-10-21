979fb5ef6259e5be764a4a0ec3ce7f5a82b76d49
devill <devillmeister@gmail.com>
2014-10-21 23:16:54 +0200

    Playing around with reading from standard input in go

--- src/dashboardControllerClient.go
+++ src/dashboardControllerClient.go
@@ -7,8 +7,15 @@ import(
 )
 
 func main() {
+	link_to_display := readMessage()
+	fmt.Println(link_to_display)
+}
+
+func readMessage() string {
 	reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Enter text: ")
-    text, _ := reader.ReadString('\n')
-    fmt.Println(text)
+    fmt.Print("Enter link: ")
+    link, _ := reader.ReadString('\n')
+    // fmt.Println(text)
+
+    return link
 }
