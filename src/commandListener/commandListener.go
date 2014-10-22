bcd4a695ce1e3558de12e526d52aafe471c7dd45
Judit Ordog-Andrasi <juci@judits-mbp.bp.prezi.private>
2014-10-22 09:49:50 +0200

    Testing file added to the commandListener

--- src/commandListener/commandListener.go
+++ src/commandListener/commandListener.go
@@ -7,15 +7,14 @@ import(
 )
 
 func main() {
-	link_to_display := readMessage()
+	link_to_display := ReadMessage()
 	fmt.Println(link_to_display)
 }
 
-func readMessage() string {
+func ReadMessage() string {
 	reader := bufio.NewReader(os.Stdin)
     fmt.Print("Enter link: ")
     link, _ := reader.ReadString('\n')
-    // fmt.Println(text)
 
     return link
 }
---
+++ src/commandListener/commandListener_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestReadMessage(t *testing.T) {
+
+}
