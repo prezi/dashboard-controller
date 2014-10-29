660724f3ed80fca38c9f17eb458836b4da075fd7
Judit Ordog-Andrasi <knifeofdreams86@gmail.com>
2014-10-29 11:32:16 +0100

    Master code cleaned up

--- src/master/master.go
+++ src/master/master.go
@@ -3,38 +3,20 @@ package main
 import (
 	"net/http"
 	"fmt"
-//	"encoding/json"
-//	"io/ioutil"
-//	"os"
 )
 
-//var protocol string
-var host string
-var port string
-
-type initVars struct {
-	Protocol string
-	Host string
-	Port string
-}
-
-func formatUrl (rasPiNum string) (string) {
-//	finalUrl := protocol + rasPiNum + port
+func formatUrl () (string) {
 	return "http://10.0.0.42:4000"
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	rasPiNum := r.PostFormValue("RPID")
-//	urlToPOSTToSlave := r.PostFormValue("url")
-	formattedUrl := formatUrl(rasPiNum)
-	fmt.Println(formattedUrl, "Sending", r.PostFormValue("command"), "request.") //send requests by
-	// curl localhost:4001 -X POST -d "RPID=32423&url=http://9gag.com/"
+	formattedUrl := formatUrl()
+	fmt.Println(formattedUrl, "Sending", r.PostFormValue("command"), "request.")
+	// curl localhost:5000 -X POST -d "RPID=32423&url=http://9gag.com/"
 	http.PostForm(formattedUrl, r.Form)
 }
 
-
 func main() {
-//	jsonInit()
-	http.HandleFunc("/", handler) // redirect all commands to the handler function
-	http.ListenAndServe("localhost:4001", nil) // listen for connections at port 9999 on the local machine
+	http.HandleFunc("/", handler)
+	http.ListenAndServe("localhost:5000", nil)
 }
