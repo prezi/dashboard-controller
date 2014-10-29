795e5e71ca15764b1053d5c96f709bf625b5ab78
devill <devillmeister@gmail.com>
2014-10-29 22:58:49 +0100

    Slave data handled by POST request from website is parsed and used

--- src/master/master.go
+++ src/master/master.go
@@ -2,44 +2,40 @@ package main
 
 import (
 	"net/http"
-	"fmt"
 	"io/ioutil"
-//	"log"
 	"encoding/json"
+	"fmt"
 )
 
-type RaspberryPiIP struct {
-	IP string
-}
-
-type rapsberryPiData struct {
+type Slave struct {
 	ID string
 	URL string
 }
 
-var rapsberryPiIP = RaspberryPiIP{"http://10.0.0.231:8080"}
+func handler(responseWriter http.ResponseWriter, request *http.Request) {
+	POSTrequestBody, _ := ioutil.ReadAll(request.Body)
+	var slave Slave
+	_ = json.Unmarshal(POSTrequestBody, &slave)
+	fmt.Println(slave.ID)
+	fmt.Println(slave.URL)
 
-func handler(w http.ResponseWriter, req *http.Request) {
-	body, _ := ioutil.ReadAll(req.Body)
-	fmt.Println(string(body))
-//	curl -d '{"IP":"1"}' -H "Content-Type: application/json" http://localhost:5000
-//returns: 2014/10/29 21:57:39 {"IP":"1"}
-
-	var rpip RaspberryPiIP
-	_ = json.Unmarshal(body, &rpip)
-	fmt.Println(rpip.IP)
-
-
-//	fmt.Println(req.PostFormValue("URL"))
-//	fmt.Println(req.PostFormValue("URL"))
-	// curl -X POST -d "URL=http://google.com" http://localhost:5000
-	// returns: http://google.com
-}
-
-func main() {
 	raspberryPiIP := make(map[string]string)
 	raspberryPiIP["1"] = "http://10.0.0.42:8080"
 	raspberryPiIP["2"] = "http://10.0.0.231:8080"
+
+	var slaveAddress string
+	if slave.ID == "1" {
+		slaveAddress = raspberryPiIP["1"]
+	}  else if slave.ID == "2" {
+		slaveAddress = raspberryPiIP["2"]
+	}
+
+	fmt.Println(slaveAddress)
+//	http.PostForm(slaveAddress, request.Form)
+}
+
+func main() {
+
 
 	http.HandleFunc("/", handler)
 	http.ListenAndServe("localhost:5000", nil)
