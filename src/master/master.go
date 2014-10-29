f76fb2f6fdfd32f8b6b336feadf0d51caa460219
devill <devillmeister@gmail.com>
2014-10-29 22:21:08 +0100

    Master now reads JSON input sent in POST request

--- src/master/master.go
+++ src/master/master.go
@@ -3,20 +3,37 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"io/ioutil"
+//	"log"
+	"encoding/json"
 )
 
 type RaspberryPiIP struct {
 	IP string
 }
 
+type rapsberryPiData struct {
+	ID string
+	URL string
+}
+
 var rapsberryPiIP = RaspberryPiIP{"http://10.0.0.231:8080"}
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Sending POST request to", rapsberryPiIP.IP, "with url", r.Form)
-	// curl localhost:5000 -X POST -d "RPID=32423&url=http://9gag.com/"
+func handler(w http.ResponseWriter, req *http.Request) {
+	body, _ := ioutil.ReadAll(req.Body)
+	fmt.Println(string(body))
+//	curl -d '{"IP":"1"}' -H "Content-Type: application/json" http://localhost:5000
+//returns: 2014/10/29 21:57:39 {"IP":"1"}
 
-	fmt.Println(r)
-	http.PostForm(rapsberryPiIP.IP, r.Form)
+	var rpip RaspberryPiIP
+	_ = json.Unmarshal(body, &rpip)
+	fmt.Println(rpip.IP)
+
+
+//	fmt.Println(req.PostFormValue("URL"))
+//	fmt.Println(req.PostFormValue("URL"))
+	// curl -X POST -d "URL=http://google.com" http://localhost:5000
+	// returns: http://google.com
 }
 
 func main() {
