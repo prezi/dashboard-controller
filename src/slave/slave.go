e0210c13c2551b67638a8006cb8651f4947d67b2
Liana Lo <liana.lo@prezi.com>
2014-10-29 12:41:15 +0100

    dynamic blocking solution

--- src/slave/slave.go
+++ src/slave/slave.go
@@ -8,7 +8,7 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
-	"time"
+	"bytes"
 )
 
 const DEFAULT_LOCALHOST_PORT = 8080
@@ -83,23 +83,42 @@ func setOS() {
 	}
 }
 
+func blockProgramWhileBrowserCloses() {
+	// block the code so that the browser can finish closing 
+	// use a while-loop to check if there is a browser process running, 
+	// exit the loop once all browser processes have closed
+
+	var existingProcess []byte
+	emptyByteArray := make([]byte, 0)
+	
+	for {
+		switch OS {
+		case "Linux":
+			existingProcess, err = exec.Command("pgrep", "chromium").CombinedOutput()		
+		case "OS X":
+			existingProcess, err = exec.Command("pgrep", "Google Chrome").CombinedOutput()
+		}
+
+		if bytes.Equal(existingProcess, emptyByteArray) { 
+				break
+		}
+	}
+}
+
 func killBrowser() {
 	switch OS {
 	case "Linux":
-		fmt.Printf("Executing command: killall chromium")
-		err = exec.Command("killall", "chromium").Run() // TODO: this needs testing
+		fmt.Println("Executing command: killall chromium")
+		err = exec.Command("killall", "chromium").Run() 
 	case "OS X":
-		fmt.Printf("Executing command: killall 'Google Chrome'\n")
+		fmt.Println("Executing command: killall 'Google Chrome'")
 		err = exec.Command("killall", "Google Chrome").Run()
 	}
 
 	if err != nil {
 		fmt.Printf("Error killing current browser: %v\n", err)
 	} else {
-	// sleep the code so that the browser can finish closing 
-	// use a while loop to check if there is a Google Chrome process running, 
-	// do not exit the loop until Google Chrome has closed
-	time.Sleep(1 * time.Second)		
+		blockProgramWhileBrowserCloses()	
 	}
 }
 
