2bc7fd89e1d0d0c9c49a17b733989369f5b9cda7
Judit Ordog-Andrasi <knifeofdreams86@gmail.com>
2014-10-31 13:32:35 +0100

    Dead code eliminated from webserver code, code reformatted, variable names changed

--- src/webserver/webserver.go
+++ src/webserver/webserver.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"strings"
-	"fmt"
 	"net/http"
 	"log"
 	"mime"
@@ -13,19 +12,19 @@ import (
 )
 
 type Message struct {
-	ID string
+	ID  string
 	URL string
 }
 
 type Reply struct {
 	Code string
-	URL string
-	ID string
+	URL  string
+	ID   string
 }
 
 func statusCode(link string) (int) {
 	response, err := http.Head(link)
-	if (err!=nil) {
+	if (err != nil) {
 		return 0
 	} else {
 		return response.StatusCode
@@ -33,90 +32,63 @@ func statusCode(link string) (int) {
 }
 
 func sendMaster(url, id string) {
-
-		m := Message{id, url}
-		jsonMessage, err := json.Marshal(m)
-		if (err!=nil) {
-			log.Fatal(err)
-		}
-		fmt.Println(string(jsonMessage))
-
-    	client := &http.Client{}
-    	resp, err := client.Post("http://localhost:5000", "application/json", strings.NewReader(string(jsonMessage)))
-    	if err != nil {
-       		panic(err)
-    	}
-    	defer resp.Body.Close()
-}
-
-func reply(URL,status_code, rb_ID string) ([]byte) {
-
-	r:=Reply{status_code,URL,rb_ID}
-	jsonMessage, err := json.Marshal(r)
-	if err!=nil {
+	m := Message{id, url}
+	json_message, err := json.Marshal(m)
+	if (err != nil) {
 		log.Fatal(err)
 	}
-	return jsonMessage
+
+	client := &http.Client{}
+	response, err := client.Post("http://localhost:5000", "application/json", strings.NewReader(string(json_message)))
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
 }
 
-func sendInfo(w http.ResponseWriter,status_code string,URL string,rb_ID string) {
-	replyMessage:=reply(URL,status_code,rb_ID)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(replyMessage)	
+func reply(URL, status_code, slave_ID string) ([]byte) {
+	reply := Reply{status_code, URL, slave_ID}
+	json_message, err := json.Marshal(reply)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return json_message
 }
 
-func formHandler(w http.ResponseWriter, r *http.Request) {
+func sendInfo(response_writer http.ResponseWriter, status_code string, URL string, slave_ID string) {
+	reply_Message := reply(URL, status_code, slave_ID)
+	response_writer.Header().Set("Content-Type", "application/json")
+	response_writer.Write(reply_Message)
+}
 
-	if r.Method == "GET" {
-		if (r.URL.Path=="/"){
-			r.URL.Path+="form.html"
+func formHandler(response_writer http.ResponseWriter, request *http.Request) {
+	if request.Method == "GET" {
+		if (request.URL.Path == "/") {
+			request.URL.Path+="form.html"
 		}
-		mimetype:=mime.TypeByExtension(filepath.Ext(r.URL.Path[1:]))
-		w.Header().Set("Content-type", mimetype)
-		t, err := template.ParseFiles(r.URL.Path[1:]) 
-		if (err!=nil){
+		mime_type := mime.TypeByExtension(filepath.Ext(request.URL.Path[1:]))
+		response_writer.Header().Set("Content-type", mime_type)
+		template, err := template.ParseFiles(request.URL.Path[1:])
+		if (err != nil) {
 			log.Fatal(err)
 		} else {
-			t.Execute(w, nil)
-		}	
-	} else {
-		URL:=r.FormValue("url")
-		rb_ID:=r.FormValue("rb-id")
-		fmt.Println(URL,rb_ID)
-		fmt.Println(statusCode(URL))
-
-		m := Message{rb_ID, URL}
-		jsonMessage, err := json.Marshal(m)
-		if (err!=nil) {
-			log.Fatal(err)
+			template.Execute(response_writer, nil)
 		}
-
-		fmt.Println(string(jsonMessage))
-    	client := &http.Client{}
-    	resp, err := client.Post("http://localhost:5000", "application/json", strings.NewReader(string(jsonMessage)))
-    	if err != nil {
-       		panic(err)
-    	}
-    	defer resp.Body.Close()
-
-		//http.Post("http://10.0.0.114:5000", string(b))
-		http.Redirect(w,r,"/form.html",301)
 	}
-	
 }
 
-func submitHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method=="POST" {
-		URL:=r.FormValue("url")
-		rb_ID:=r.FormValue("rb-id")
-		status_code:=statusCode(URL)
-		sendMaster(URL,rb_ID)
-		sendInfo(w,strconv.Itoa(status_code),URL,rb_ID)
+func submitHandler(response_writer http.ResponseWriter, request *http.Request) {
+	if request.Method == "POST" {
+		URL := request.FormValue("url")
+		slave_ID := request.FormValue("rb-id")
+		status_code := statusCode(URL)
+		sendMaster(URL, slave_ID)
+		sendInfo(response_writer, strconv.Itoa(status_code), URL, slave_ID)
 	}
 }
 
 func main() {
-	http.HandleFunc("/",formHandler)
-	http.HandleFunc("/form-submit",submitHandler)
+	http.HandleFunc("/", formHandler)
+	http.HandleFunc("/form-submit", submitHandler)
 	http.ListenAndServe("localhost:4003", nil)
 }
