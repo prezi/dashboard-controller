3eb88052de6e10a987c0838fec3ebef53644c12d
Matyas Fodor <matyas.fodor@gmail.com>
2014-11-03 15:14:33 +0100

    code.js changed

--- src/webserver/code.js
+++ src/webserver/code.js
@@ -1,26 +1,4 @@
-/*
-function submitForm() {
-	alert("sfgbf");
-    var http = new XMLHttpRequest();
-    http.open("POST", "/form-submit", true);
-    http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    var params = "search="; // probably use document.getElementById(...).value
-    http.send(params);
-    http.onload = function() {
-        alert(http.responseText);
-    }
-}
-*/
-
 $( document ).ready(function() {
-    //alert("Loaded");
-    /*	
-    $('#mainform').submit(function () {
-	 alert("WATWAT");
-	 return false;
-	});
-	*/
-	//$("#manual-example a[rel=tipsy]").tipsy("show");
 	$("#manual-example a[rel=tipsy]").show();
 	$("#mainform").submit(function(e)
 	{
@@ -33,7 +11,7 @@ $( document ).ready(function() {
 	        data : postData,
 	        success:function(data, textStatus, jqXHR) 
 	        {
-	            //alert("Success");
+	        	//TO DO: Change it to universal info box!
 	            $( "#rb_id" ).find( "#value" ).html(data.ID);
 	            $( "#url" ).find( "#value" ).html(data.URL);
 	            $( "#statuscode" ).find( "#value" ).html(data.Code);
@@ -44,10 +22,10 @@ $( document ).ready(function() {
 	        },
 	        error: function(jqXHR, textStatus, errorThrown) 
 	        {
-	            //alert("Fail"); 
+	            //TO DO: As above, change it to universal info box
 	        }
 	    });
-	    e.preventDefault(); //STOP default action
+	    e.preventDefault(); 
 	    //e.unbind(); //unbind. to stop multiple form submit.
 	});
 
