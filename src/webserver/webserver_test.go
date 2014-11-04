11fce54a6ce2e0d240c4a5e77d018b705ceb6688
Eniko BakosDemeter <enidemi@gmail.com>
2014-11-04 15:06:25 +0100

    partial set mimetype test added

--- src/webserver/webserver_test.go
+++ src/webserver/webserver_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"testing"
 	"github.com/stretchr/testify/assert"
+	// "fmt"
 )
 
 func TestStatusCode(t *testing.T) {
@@ -15,3 +16,36 @@ func TestStatusCode(t *testing.T) {
 
 	assert.Equal(t, 200, responseStatusCode)
 }
+
+func TestSetMimeType(t *testing.T) {
+	var headerContentType = ""
+	
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
+		setMimeType(w)
+		headerContentType = w.Header().Get("Content-type")
+	}))
+
+	sendHeadRequestTo(testServer.URL)
+
+	assert.Equal(t, "text/html", headerContentType)
+}
+
+func setMimeType(responseWriter http.ResponseWriter) {
+	responseWriter.Header().Set("Content-type", "text/css")
+}
+
+
+func sendHeadRequestTo(url string) {
+	client := &http.Client{}
+
+	_,_ = client.Head(url)
+
+}
+
+// func TestReply(t *testing.T) {
+
+// }
+
+// func TestSendInfo(t *testing.T) {
+
+// }
