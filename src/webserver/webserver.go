2869c20345fb1e7b0c0baac9ecb1875a7c564b2f
Eniko BakosDemeter <enidemi@gmail.com>
2014-11-18 15:35:29 +0100

    descriptiveFunctionNamesPartOne

--- src/webserver/webserver.go
+++ src/webserver/webserver.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	"strings"
-	"net/http"
-	"log"
-	"path"
-	"html/template"
-	"encoding/json"
-	"strconv"
 	"bytes"
+	"encoding/json"
 	"fmt"
+	"html/template"
+	"net/http"
+	"path"
+	"strconv"
+	"strings"
 )
 
-var MASTER_URL ="http://localhost:5000"
+var MASTER_URL = "http://localhost:5000"
 var TEMPLATE_PATH = "src/webserver/templates/"
 var STATIC_PATH = "src/webserver/static"
 
@@ -39,90 +38,85 @@ var id_list = IdList{
 	Id: []string{"1", "2"},
 }
 
-func statusCode(link string) (int) {
+func main() {
+	fs := http.FileServer(http.Dir(STATIC_PATH))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", formHandler)
+	http.HandleFunc("/form-submit", submitHandler)
+	http.HandleFunc("/receive_slave", receiveAndMapSlaveAddress)
+	http.ListenAndServe(":4003", nil)
+}
+
+func formHandler(response_writer http.ResponseWriter, request *http.Request) {
+	if request.Method == "GET" {
+		if request.URL.Path != "/" {
+			http.Redirect(response_writer, request, "/", 301)
+		}
+		template, err := template.ParseFiles(path.Join(TEMPLATE_PATH, "form.html"))
+		if err != nil {
+			fmt.Println(err)
+		}
+		template.Execute(response_writer, id_list)
+	}
+}
+
+func submitHandler(response_writer http.ResponseWriter, request *http.Request) {
+	if request.Method == "POST" {
+		urlToDisplay := request.FormValue("url")
+		slave_ID := request.FormValue("slave-id")
+		status_code := checkStatusCode(urlToDisplay)
+		sendUrlAndIdToMaster(MASTER_URL, urlToDisplay, slave_ID)
+		sendConfirmationMessageToUser(response_writer, strconv.Itoa(status_code), urlToDisplay, slave_ID)
+	}
+}
+
+func checkStatusCode(link string) int {
 	response, err := http.Head(link)
-	if (err != nil) {
+	if err != nil {
 		return 0
 	} else {
 		return response.StatusCode
 	}
 }
 
-func sendMaster(masterUrl,urlToDisplay, id string) (int) {
+func sendUrlAndIdToMaster(masterUrl, urlToDisplay, id string) error {
 	m := Message{id, urlToDisplay}
 	json_message, err := json.Marshal(m)
-	if (err != nil) {
-		log.Fatal(err)
-		return 0
+	if err != nil {
+		fmt.Println(err)
+		return err
 	}
 	client := &http.Client{}
 	response, err := client.Post(masterUrl, "application/json", strings.NewReader(string(json_message)))
 	if err != nil {
 		fmt.Println(err)
-		return 0
-		//TODO return error? 
+		return err
 	}
 	defer response.Body.Close()
-	return 1
+	return nil
 }
 
-func reply(URL, status_code, slave_ID string) ([]byte) {
-	// reply := Reply{status_code, URL, slave_ID}
-	t, err := template.ParseFiles(path.Join(TEMPLATE_PATH,"infobox.html"))		
-	if (err != nil) {
-			log.Fatal(err)
-	} 
+func sendConfirmationMessageToUser(response_writer http.ResponseWriter, status_code, URL, slave_ID string) {
+	confirmationMessage := confirmationMessage(URL, status_code, slave_ID)
+	header := response_writer.Header()
+	header.Set("Content-Type", "application/json")
+	response_writer.Write(confirmationMessage)
+}
+
+func confirmationMessage(URL, status_code, slave_ID string) []byte {
+	t, err := template.ParseFiles(path.Join(TEMPLATE_PATH, "infobox.html"))
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	buf := new(bytes.Buffer)
 	t.ExecuteTemplate(buf, "T", StatusMessage{status_code, URL, slave_ID})
-	
-	json_message, err := json.Marshal(Reply{HTML:buf.String()})
+
+	jsonMessage, err := json.Marshal(Reply{HTML: buf.String()})
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
-	return json_message
-}
-
-func sendInfo(response_writer http.ResponseWriter, status_code , URL , slave_ID string) {
-	reply_Message := reply(URL, status_code, slave_ID)
-	header:=response_writer.Header()
-	header.Set("Content-Type", "application/json")
-	//log.Fatal("sth")
-	response_writer.Write(reply_Message)
-}
-
-func formHandler(response_writer http.ResponseWriter, request *http.Request) {
-	if request.Method == "GET" {
-		//if (request.URL.path)
-		//fmt.Println(request.URL.Path)
-		if (request.URL.Path!="/") {
-			http.Redirect(response_writer,request,"/",301)
-		}
-		template, err := template.ParseFiles(path.Join(TEMPLATE_PATH,"form.html"))
-		if (err != nil) {
-    		http.Error(response_writer, http.StatusText(500), 500)
-			log.Fatal(err)
-		} 
-		template.Execute(response_writer, id_list) 
-	}
-}
-
-func submitHandler(response_writer http.ResponseWriter, request *http.Request) {
-	if request.Method == "POST" {
-		urlToDisplay := request.FormValue("url") 
-		slave_ID := request.FormValue("slave-id") 
-		status_code := statusCode(urlToDisplay)
-		//c := make(chan int)
-		// go func() {
-	 //    	c <- sendMaster(MASTER_URL,urlToDisplay, slave_ID) 
-  //   	}()
-  //   	ccc := <-c
-  //   	if (ccc == 1) {
-		// 	sendInfo(response_writer, strconv.Itoa(status_code), urlToDisplay, slave_ID)
-		// }
-		sendMaster(MASTER_URL,urlToDisplay, slave_ID)
-		sendInfo(response_writer, strconv.Itoa(status_code), urlToDisplay, slave_ID)
-	}
+	return jsonMessage
 }
 
 func receiveAndMapSlaveAddress(_ http.ResponseWriter, request *http.Request) {
@@ -130,14 +124,4 @@ func receiveAndMapSlaveAddress(_ http.ResponseWriter, request *http.Request) {
 	fmt.Printf("\nNEW SLAVE RECEIVED.\n")
 	fmt.Println("Slave Name: ", slaveName)
 	id_list.Id = append(id_list.Id, slaveName)
-}
-
-func main() {
-	fs := http.FileServer(http.Dir(STATIC_PATH))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.HandleFunc("/", formHandler)
-	http.HandleFunc("/form-submit", submitHandler)
-	http.HandleFunc("/receive_slave", receiveAndMapSlaveAddress)
-	http.ListenAndServe("localhost:4003", nil)
 }
--- src/webserver/webserver_test.go
+++ src/webserver/webserver_test.go
@@ -17,107 +17,9 @@ type Slave struct {
 	URL string
 }
 
-// type MockResponseWriter interface {
-//         Header() http.Header
-//         Write([]byte) (int, error)
-//  //       WriteHeader(int)
-// }
-
-func setUpTestServerWithPath(path string) (headerContentType string) {
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
-		//setMimeType(w, path)
-		headerContentType = w.Header().Get("Content-type")
-	}))
-	sendHeadRequestTo(testServer.URL)
-	return
-}
-
-func sendHeadRequestTo(url string) {
-	client := &http.Client{}
-	_, _ = client.Head(url)
-}
-
-func parseJsonSlave(input []byte) (slave Slave) {
-	err := json.Unmarshal(input, &slave)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-	return
-}
-
-func parseJsonReply(input []byte) (reply Reply) {
-	err := json.Unmarshal(input, &reply)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-	return
-}
-
-func TestStatusCode(t *testing.T) {
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
-	}))
-
-	responseStatusCode := statusCode(testServer.URL)
-
-	assert.Equal(t, 200, responseStatusCode)
-	responseStatusCode = statusCode("")
-
-	assert.Equal(t, 0, responseStatusCode)
-}
-
-func TestSendMaster(t *testing.T) {
-	var numberOfMessagesSent = 0
-	var url = ""
-	var id = ""
-
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
-		numberOfMessagesSent++
-		POSTRequestBody, _ := ioutil.ReadAll(request.Body)
-		defer request.Body.Close()
-		slave := parseJsonSlave(POSTRequestBody)
-		url = slave.URL
-		id = slave.ID
-
-	}))
-
-	returnValue := sendMaster(testServer.URL, "http://index.hu", "2")
-
-	assert.Equal(t, 1, numberOfMessagesSent)
-	assert.Equal(t, "http://index.hu", url)
-	assert.Equal(t, "2", id)
-	assert.Equal(t, 1, returnValue)
-}
-
-func TestReply(t *testing.T) {
-	TEMPLATE_PATH="templates/"
-	answer_string := parseJsonReply(reply("aaaa", "bbbb", "cccc")).HTML
-	assert.Equal(t, true, strings.Contains(answer_string, "aaaa"))
-	assert.Equal(t, true, strings.Contains(answer_string, "bbbb"))
-	assert.Equal(t, true, strings.Contains(answer_string, "cccc"))
-}
-
-func TestSendInfo(t *testing.T) {
-	//mock_response_writer = MockResponseWriter;
-	//var mock_response_writer http.ResponseWriter
-	TEMPLATE_PATH="templates/"
-	var responseHeader string
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
-		sendInfo(w,"aaaa", "bbbb", "cccc")
-		responseHeader=w.Header().Get("Content-Type")
-	}))
-	//sendHeadRequestTo(testServer.URL)
-	client := &http.Client{}
-	resp, _ := client.Get(testServer.URL)
-
-	assert.Equal(t, 200, resp.StatusCode)
-	assert.Equal(t, "application/json", responseHeader)
-	POSTRequestBody, _ := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	reply := parseJsonReply(POSTRequestBody).HTML
-
-	assert.Equal(t, true, strings.Contains(reply, "aaaa"))
-	assert.Equal(t, true, strings.Contains(reply, "bbbb"))
-	assert.Equal(t, true, strings.Contains(reply, "cccc"))
+func TestFormHandler(t *testing.T) {
+	assert.Equal(t, 200, sendGetToFormHandler("/"))
+	assert.Equal(t, 301, sendGetToFormHandler("addfs"))
 }
 
 func sendGetToFormHandler(URL string) (int) {
@@ -133,11 +35,6 @@ func sendGetToFormHandler(URL string) (int) {
 	return resp.StatusCode
 }
 
-func TestFormHandler(t *testing.T) {
-	assert.Equal(t, 200, sendGetToFormHandler("/"))
-	assert.Equal(t, 301, sendGetToFormHandler("addfs"))
-}
-
 func TestSubmitHandler(t *testing.T) {
 	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
 		submitHandler(w,request)
@@ -151,6 +48,82 @@ func TestSubmitHandler(t *testing.T) {
 	reply := parseJsonReply(POSTRequestBody).HTML
 
 	assert.Equal(t, true, strings.Contains(reply, "http://www.google.com"))
+}
+
+func TestCheckStatusCode(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
+	}))
+
+	responseStatusCode := checkStatusCode(testServer.URL)
+	assert.Equal(t, 200, responseStatusCode)
+	responseStatusCode = checkStatusCode("")
+	assert.Equal(t, 0, responseStatusCode)
+}
+
+func TestSendUrlAndIdToMaster(t *testing.T) {
+	var numberOfMessagesSent = 0
+	var url = ""
+	var id = ""
+
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
+		numberOfMessagesSent++
+		POSTRequestBody, _ := ioutil.ReadAll(request.Body)
+		defer request.Body.Close()
+		slave := parseJsonSlave(POSTRequestBody)
+		url = slave.URL
+		id = slave.ID
+
+	}))
+	
+	sendUrlAndIdToMaster(testServer.URL, "http://index.hu", "2")
+	assert.Equal(t, 1, numberOfMessagesSent)
+	assert.Equal(t, "http://index.hu", url)
+	assert.Equal(t, "2", id)
+}
+
+func parseJsonSlave(input []byte) (slave Slave) {
+	err := json.Unmarshal(input, &slave)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	return
+}
+
+func TestSendConfirmationMessageToUser(t *testing.T) {
+	TEMPLATE_PATH="templates/"
+	var responseHeader string
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
+		sendConfirmationMessageToUser(w,"aaaa", "bbbb", "cccc")
+		responseHeader=w.Header().Get("Content-Type")
+	}))
+	client := &http.Client{}
+	resp, _ := client.Get(testServer.URL)
+
+	assert.Equal(t, 200, resp.StatusCode)
+	assert.Equal(t, "application/json", responseHeader)
+	POSTRequestBody, _ := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	reply := parseJsonReply(POSTRequestBody).HTML
+
+	assert.Equal(t, true, strings.Contains(reply, "aaaa"))
+	assert.Equal(t, true, strings.Contains(reply, "bbbb"))
+	assert.Equal(t, true, strings.Contains(reply, "cccc"))
+}
+
+func parseJsonReply(input []byte) (reply Reply) {
+	err := json.Unmarshal(input, &reply)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	return
+}
+
+func TestConfirmationMessage(t *testing.T) {
+	TEMPLATE_PATH="templates/"
+	answer_string := parseJsonReply(confirmationMessage("aaaa", "bbbb", "cccc")).HTML
+	assert.Equal(t, true, strings.Contains(answer_string, "aaaa"))
+	assert.Equal(t, true, strings.Contains(answer_string, "bbbb"))
+	assert.Equal(t, true, strings.Contains(answer_string, "cccc"))
 }
 
 func TestReceiveAndMapSlaveAddress(t *testing.T) {
