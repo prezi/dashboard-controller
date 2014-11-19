8d6952980338b02574198463a02bcc2b0b6c4ba0
Matyas Fodor <matyas.fodor@gmail.com>
2014-11-19 16:30:29 +0100

    refractored tests

--- src/master/masterModules/slaveMap_test.go
+++ src/master/masterModules/slaveMap_test.go
@@ -10,33 +10,11 @@ import (
 	"sort"
 )
 
-
 func TestInitializeSlaveIPs(t *testing.T) {
 	slaveIPMap := initializeSlaveIPs()
 
 	assert.Equal(t, "http://10.0.0.122:8080", slaveIPMap["1"])
 	assert.Equal(t, "http://10.0.1.11:8080", slaveIPMap["2"])
-}
-
-func TestReceiveAndMapSlaveAddress(t *testing.T) {
-	// name := ""
-	// testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
-	// 	name = request.PostFormValue("slaveName")
-	// }))
-
-	// error := sendSlaveToWebserver([]string{testServer.URL, "/receive_slave"}, "ApplePie")
-
-	// assert.Equal(t, "ApplePie", name)
-	// assert.Nil(t, error)
-}
-
-func TestSendValidSlaveToWebserver(t *testing.T) {
-	// testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
-	// }))
-
-	// error := sendSlaveToWebserver([]string{testServer.URL, "/receive_slave"},  "FantasticName")
-
-	// assert.Nil(t, error)
 }
 
 func TestPrintServerConfirmation(t *testing.T) {
@@ -53,12 +31,10 @@ func TestSendSlaveToWebserver(t *testing.T) {
 		returnedIds = idlist.Id
 
 	}))
-	slaveIPs := make(map[string]string)
-	slaveIPs["1"] = "http://10.0.0.122:8080"
-	slaveIPs["2"] = "http://10.0.1.11:8080"
+	slaveIPs := initializeSlaveIPs()
 	sendSlaveToWebserver([]string{testServer.URL}, slaveIPs)
 	validIdList := []string{"1","2"}
 	sort.Strings(validIdList)
 	sort.Strings(returnedIds)
-	assert.Equal(t,returnedIds,validIdList)
+	assert.Equal(t, returnedIds, validIdList)
 }
