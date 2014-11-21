54b87a486ccf2584064587aada35512a82c2cea6
Judit Ordog-Andrasi <knifeofdreams86@gmail.com>
2014-11-21 13:04:25 +0100

    Expected and actual value order corrected in setup tests

--- src/slave/slave/setUp_test.go
+++ src/slave/slave/setUp_test.go
@@ -9,9 +9,9 @@ import (
 
 func TestSetUp(t *testing.T) {
 	port, slaveName, masterURL, OS := SetUp()
-	assert.Equal(t, port, 8080)
-	assert.Equal(t, slaveName, "SLAVE NAME UNSPECIFIED")
-	assert.Equal(t, masterURL, "http://localhost:5000")
+	assert.Equal(t, 8080, port)
+	assert.Equal(t, "SLAVE NAME UNSPECIFIED", slaveName)
+	assert.Equal(t, "http://localhost:5000", masterURL)
 	assert.IsType(t, "Some OS Name", OS)
 }
 
