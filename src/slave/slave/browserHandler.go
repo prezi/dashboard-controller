654d8709acba582e14cb9cb4d95a2969c6c272a3
Eniko BakosDemeter <enidemi@gmail.com>
2014-12-11 16:56:15 +0100

    no more browser restore message

--- src/slave/slave/browserHandler.go
+++ src/slave/slave/browserHandler.go
@@ -9,24 +9,22 @@ import (
 
 func BrowserHandler(writer http.ResponseWriter, request *http.Request, OS string, browserProcess *exec.Cmd) *exec.Cmd {
 	url := request.PostFormValue("url")
-	killBrowser(OS, browserProcess)
+	killBrowser(OS)
 	browserProcess, _ = openBrowser(OS, url)
 	fmt.Fprintf(writer, "SUCCESS. \"%v\" has been posted.\n", url)
 
 	return browserProcess
 }
 
-func killBrowser(OS string, browserProcess *exec.Cmd) (err error) {
+func killBrowser(OS string) (err error) {
 	switch OS {
 	case "Linux":
 		fmt.Println("Executing command: killall chromium")
-		if browserProcess != nil {
-			if err = browserProcess.Process.Kill(); err != nil {
-				fmt.Println("Failed to kill: ", err)
-			}
-			if err = browserProcess.Wait(); err != nil {
-				fmt.Println("Error returned: ", err)
-			}
+		err = exec.Command("killall", "-TERM", "chromium").Run()
+		if err != nil {
+			fmt.Printf("Error killing current browser: %v\n", err)
+		} else {
+			blockWhileBrowserCloses(OS)
 		}
 
 	case "OS X":
@@ -67,9 +65,8 @@ func openBrowser(OS, url string) (browserProcess *exec.Cmd, err error) {
 	err = nil
 	switch OS {
 	case "Linux":
-		fmt.Printf("Executing command: chromium --kiosk %v\n", url)
-		browserProcess = exec.Command("chromium", "--kiosk", url)
-		err = browserProcess.Start()
+		fmt.Printf("Executing command: chromium --incognito --kiosk %v\n", url)
+		go exec.Command("chromium", "--incognito", "--kiosk", url).Run()
 	case "OS X":
 		fmt.Printf("Executing command: open -a 'Google Chrome' --args --kiosk %v\n", url)
 		err = exec.Command("open", "-a", "Google Chrome", "--args", "--kiosk", url).Run()
